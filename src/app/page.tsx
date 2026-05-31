a78d14595c9b8b741bb5ff5ad1435a82a2119575
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-05-31 09:32:04 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,13 +32,13 @@ export default async function Home() {
       agentName = profile.full_name;
     } else if (user.user_metadata?.full_name) {
       agentName = user.user_metadata.full_name;
+    } else if (user.email) {
+      agentName = user.email.split('@')[0];
+      agentName = agentName.charAt(0).toUpperCase() + agentName.slice(1);
     }
 
     if (user.user_metadata?.avatar_url) {
       agentAvatar = user.user_metadata.avatar_url;
-    } else if (user.email) {
-      agentName = user.email.split('@')[0];
-      agentName = agentName.charAt(0).toUpperCase() + agentName.slice(1);
     }
   }
 
