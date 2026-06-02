f7777f5ea95ebadbb5ad17bdb9d892e9d0d42009
Dilshan P.A.A.I <dilshaprathapaarachchi@gmail.com>
2026-06-02 19:48:20 +0530

    changed file name

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,25 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/api/icon?size=192",
         sizes: "192x192",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon-192x192.png",
+        src: "/api/icon?size=192",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/api/icon?size=512",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/api/icon?size=512",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
